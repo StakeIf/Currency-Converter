184b985f6c96b8d6fcdf229586f59ccec751dc51
StakeIf <krasnov2002kolya@mail.ru>
2024-04-09 12:42:35 +0700

    Добавлен тест для конвертации Доллара в Рубль за 08.04.2024

--- test_Converter.py
+++ test_Converter.py
@@ -1,8 +1,19 @@
 import unittest
 from Converter import Converter
 
-class ListHandlerTests(unittest.TestCase):
+class ConverterTests(unittest.TestCase):
 
     def test_ClassCreation(self):
         converter = Converter()
         self.assertIsNotNone(converter)
+
+    def test_converte_USDtoRUB20240408_9212(self):
+        converter = Converter()
+        fromCurrency = 'USD'
+        toCurrency = 'RUB'
+        date = '2024-04-08'
+
+        result = converter.converte(fromCurrency, toCurrency, date)
+        
+        expected = 92.19
+        self.assertEqual(result, expected)
